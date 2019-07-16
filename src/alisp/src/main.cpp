f996ae06d9e15791e975c4dbaf139ceb9c333ab1
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-07-16 23:25:00 +0200

    Cleaning things a little bit

--- src/alisp/src/main.cpp
+++ src/alisp/src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <clara.hpp>
+
 
 #include "alisp/config.hpp"
 #include "alisp/alisp/common_lexer.hpp"
@@ -6,45 +9,87 @@
 #include "alisp/alisp/parser.hpp"
 #include "alisp/alisp/error_messaging.hpp"
 
-int main(int, char *argv[])
-{
+#include "alisp/alisp/prompt.hpp"
 
+
+void eval_statement(const std::string& command);
+
+
+struct Options
+{
+    std::string eval{""};
+    std::string input{""};
+    bool version;
+};
+
+int main(int argc, char *argv[])
+{
+    using clara::Opt;
+    using clara::Arg;
+    using clara::Args;
+    using clara::Help;
+
+    bool showHelp{ false };
+    Options opts{};
+
+    auto cli = Help(showHelp)
+        | Opt(opts.version)["-v"]["--version"]("Show the version of alisp")
+        | Opt(opts.eval, "string")["-e"]["--eval"]("Input to evaluate")
+        | Arg(opts.input, "file")("Input file");
+                
+
+    const auto result = cli.parse(Args(argc, argv));
+
+    if (!result) {
+        std::cerr << "Error in command line: " << result.errorMessage() << '\n';
+    }
+            
+    if (showHelp) {
+        std::cout << cli << '\n';
+        exit(1);
+    }
+
+    if (opts.version) {
+        std::cout << "Alisp 0.0.0" << '\n';
+        exit(1);
+    }
+
+    if(!opts.input.empty()){
+        std::cout << "input" << "\n";        
+        return 0;
+    }
+
+    if(!opts.eval.empty()){
+        eval_statement(opts.eval);
+        return 0;
+    }
+
+
+    alisp::prompt::init();
+    alisp::prompt::repl(">>> ");
     
     
+    return 0;
+}
+
+
+
+void eval_statement(const std::string& command)
+{
 
     alisp::ErrorMessanger err;
     alisp::ALLexer lex{err};
-
-
-    std::string input{R"(
- (defun (a b c &optional)
-    "this is sick"
-    (setq a b)
-    (if a (setq b 3) (setq c 4))
-    (progn
-       (message "Hello World!")
-       (if a (setq b 3) (setq c 4))))
-)"};
-
-
-    err.set_input(input);
-    auto toks = lex.tokenize(input);
-
-
-    // err.set_input(argv[1]);
-    // auto toks = lex.tokenize(argv[1]);
-
+    err.set_input(command);
     
+    auto toks = lex.tokenize(command);
     alisp::ALParser pars{toks};    
+    
     auto res = pars.parseWhole();
+
     for (auto r : res) {
         std::cout << ":";
         alisp::printObject(r);
         std::cout << "\n";
     }
-
-
-  
-    return 0;
 
 }
