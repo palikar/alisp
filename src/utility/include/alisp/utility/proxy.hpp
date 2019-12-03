26e7abe3ca05365c89e2e63517fb5735dbc099cc
Arnaudov, Stanislav Tomov <stanislav.arnaudov@iosb.fraunhofer.de>
2019-12-03 18:16:09 +0100

    Extenalizing the for_each for tuples

--- src/utility/include/alisp/utility/proxy.hpp
+++ src/utility/include/alisp/utility/proxy.hpp
@@ -4,52 +4,40 @@
 #include <iostream>
 #include <iterator>
 
+#include "alisp/utility/helpers.hpp"
+
 namespace alisp::utility
 {
 namespace detail
 {
 
-
-template<std::size_t I = 0, typename FuncT, typename... Tp>
-inline std::enable_if_t<I == sizeof...(Tp), void>
-for_each(std::tuple<Tp...> &, FuncT)
-{ }
-
-template<std::size_t I = 0, typename FuncT, typename... Tp>
-inline std::enable_if_t<I < sizeof...(Tp), void>
-for_each(std::tuple<Tp...>& t, FuncT f)
-{
-    f(std::get<I>(t));
-    for_each<I + 1, FuncT, Tp...>(t, f);
-};
-    
-
 template<typename T, typename...Tracker>
-struct proxy{
+struct proxy
+{
 
   public:
 
-    explicit proxy(T& obj, Tracker && ... track) :
+    proxy(T& obj, Tracker && ... track) :
         m_obj(&obj), m_track(std::forward<decltype(track)>(track)...)  {
         (track.before(obj), ...);
         
     }
 
     ~proxy(){
-        detail::for_each(m_track, [&](auto& tracker){tracker.after(*m_obj);});
+        ::alisp::utility::for_each(m_track, [&](auto& tracker){tracker.after(*m_obj);});
     }
 
     T* operator ->() { return m_obj;}
 
   private:
     T* m_obj;
-    std::tuple<Tracker ...> m_track;
-};
+    std::tuple<Tracker ...> m_track;};
 
 }
 
 template<typename T, typename ...Tracker>
-class Tracked : public Tracker ... {
+class Tracked : public Tracker ...
+{
   public :
     explicit Tracked(T& obj) : m_obj(obj) {}
 
@@ -61,6 +49,5 @@ class Tracked : public Tracker ... {
     T& m_obj;
 
 };
-
 
 }
