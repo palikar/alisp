68197fd03de68656b0acea8b971aa2a536e0939e
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-12-17 23:41:40 +0100

    Adding s bunch of list functions

--- src/alisp/src/alisp_lists.cpp
+++ src/alisp/src/alisp_lists.cpp
@@ -31,4 +31,64 @@ ALObject* Fmapc(ALObject* obj, env::Environment*, eval::Evaluator* eval)
 }
 
 
+
+ALObject* Fcar(ALObject* obj, env::Environment*, eval::Evaluator* eval)
+{
+    assert_size<1>(obj);
+    auto list = eval->eval(obj->i(0));
+    return list->i(0);
+}
+
+
+ALObject* Fcons(ALObject* obj, env::Environment*, eval::Evaluator* eval)
+{
+    assert_size<1>(obj);
+    auto list = eval->eval(obj->i(0));
+    assert_list(list);
+    return splice(list, 1);
+}
+
+ALObject* Fhead(ALObject* obj, env::Environment* env, eval::Evaluator* eval)
+{
+    return Fcar(obj, env, eval);
+}
+
+
+ALObject* Ftail(ALObject* obj, env::Environment* env, eval::Evaluator* eval)
+{
+    return Fcons(obj, env, eval);
+}
+
+ALObject* Flast(ALObject* obj, env::Environment*, eval::Evaluator* eval)
+{
+    assert_size<1>(obj);
+    auto list = eval->eval(obj->i(0));
+    assert_list(list);
+    return list->i(list->length() - 1);
+}
+
+
+ALObject* Finit(ALObject* obj, env::Environment*, eval::Evaluator* eval)
+{
+    assert_size<1>(obj);
+    auto list = eval->eval(obj->i(0));
+    assert_list(list);
+    return splice(list, 0, static_cast<ALObject::list_type::difference_type>(list->length() - 1));
+}
+
+
+ALObject* Fput(ALObject* obj, env::Environment*, eval::Evaluator* eval)
+{
+    assert_size<2>(obj);
+    auto list = eval->eval(obj->i(0));
+    assert_list(list);
+    auto element = eval->eval(obj->i(2));
+    
+    list->children().push_back(element);
+    
+    return list;
+}
+
+
+
 }
