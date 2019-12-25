fbd347c2c18626ba9b8c81f92ab46bb1cf9b15b2
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-12-25 14:21:53 +0100

    Separating the factory methods

---
+++ src/alisp/include/alisp/alisp/alisp_factory.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <variant>
+#include <vector>
+#include <iterator>
+#include <memory>
+
+#include "alisp/alisp/alisp_common.hpp"
+#include "alisp/utility.hpp"
+
+
+
+
+namespace alisp
+{
+
+/*   ____                _   _               _          _                 */
+/*  / ___|_ __ ___  __ _| |_(_) ___  _ __   | |__   ___| |_ __  _ __ ___  */
+/* | |   | '__/ _ \/ _` | __| |/ _ \| '_ \  | '_ \ / _ \ | '_ \| '__/ __| */
+/* | |___| | |  __/ (_| | |_| | (_) | | | | | | | |  __/ | |_) | |  \__ \ */
+/*  \____|_|  \___|\__,_|\__|_|\___/|_| |_| |_| |_|\___|_| .__/|_|  |___/ */
+/*                                                       |_|   */
+
+namespace detail
+{
+
+struct ALObjectHelper
+{
+
+    template<typename T>
+    static auto get(T a) -> typename  std::enable_if_t<std::is_integral_v<T>, ALObjectPtr> {
+        const auto val = static_cast<ALObject::int_type>(a); 
+        auto obj = new ALObject(val);
+
+        if (0 <= val && val <= 127) { obj->set_char_flag(); }
+        
+        return std::shared_ptr<ALObject>(obj);
+    }
+
+
+    template<typename T>
+    static auto get(T a) -> typename std::enable_if_t<std::is_floating_point_v<T>, ALObjectPtr> {
+        return std::shared_ptr<ALObject>(new ALObject(static_cast<ALObject::real_type>(a)));
+    }
+
+    
+    template<typename T>
+    static auto get(T a) -> typename std::enable_if_t<std::is_constructible_v<std::string, T>, ALObjectPtr> {
+        return std::shared_ptr<ALObject>(new ALObject(std::string(a)));
+    }
+
+    template<typename T>
+    static auto get(T a, bool) -> typename std::enable_if_t<std::is_constructible_v<std::string, T>, ALObjectPtr> {
+        return std::shared_ptr<ALObject>(new ALObject(std::string(a), true));
+    }
+
+
+    static auto get(std::vector<ALObjectPtr> vec_objs) {
+        return std::shared_ptr<ALObject>(new ALObject(vec_objs));
+    }
+
+
+    static auto get(ALObjectPtr obj) -> ALObjectPtr {
+        return obj;
+    }
+
+    template<typename ...T>
+    static auto get(T... objs) -> ALObjectPtr {
+        std::vector<ALObjectPtr> vec_objs;
+        vec_objs.reserve(sizeof...(objs));
+
+        (vec_objs.push_back(ALObjectHelper::get(objs)), ...);
+
+        return std::shared_ptr<ALObject>(new ALObject(vec_objs));
+    }
+
+};
+
+}
+
+template<typename ... T>
+inline auto make_object(T && ... args)
+{
+    return detail::ALObjectHelper::get(std::forward<T>(args) ...);
+}
+
+template<typename T>
+inline auto make_symbol(T name)
+{
+    static_assert(std::is_constructible_v<std::string, T>, "Name must be string like type.");
+    
+    return detail::ALObjectHelper::get(std::string(name), true);
+}
+
+template<typename T>
+inline auto make_int(T value)
+{
+    static_assert(std::is_integral_v<T>, "Value must be of integer type");
+
+    auto obj = make_object(static_cast<ALObject::int_type>(value));
+    return obj;
+}
+
+template<typename T>
+inline auto make_double(T value)
+{
+    static_assert(std::is_arithmetic_v<T>, "Value must be of real type");
+    return make_object(static_cast<double>(value));
+}
+
+inline auto make_string(std::string value)
+{
+    return make_object(value);
+}
+
+inline auto make_list(ALObjectPtr obj)
+{
+    return detail::ALObjectHelper::get(std::vector{obj});
+}
+
+}
