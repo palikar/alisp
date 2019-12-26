fc669710144bdafe6a8fc1e0687daf47ae77db55
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-12-26 15:26:50 +0100

    Clearing the bugs with the logs

--- src/utility/include/alisp/utility/logging.hpp
+++ src/utility/include/alisp/utility/logging.hpp
@@ -1,17 +1,8 @@
 #pragma once
 
-#include <memory>
-#include <string>
-#include <vector>
-#include <iterator>
-#include <string>
-#include <ostream>
-#include <sstream>
-#include <functional>
 #include <iostream>
-
-// #include "alisp/config.hpp"
-
+#include <string>
+#include <cstring>
 
 namespace alisp::logging
 {
@@ -29,25 +20,25 @@ enum class LogCategory
 
 struct LogEntry
 {
-    std::string file_name; 
-    std::string function_name; 
-    int line_number; 
-    std::stringstream message;
+    std::string file_name;
+    std::string function_name;
+    int line_number;
+    std::string message;
 };
 
 
 template <LogCategory t_category>
-class StdLog
+struct StdLog
 {
     static const LogCategory category = t_category;
 
     void log(LogEntry t_entry) {
-        std::cout << '[' << t_entry.file_name << ':' << t_entry.function_name << ':' << t_entry.line_number << ']'
-                  <<  t_entry.message.str ( ) << "\n";
+        std::cout << '[' << t_entry.file_name << ':' << t_entry.function_name << ':' << t_entry.line_number << "] "
+                  <<  t_entry.message << "\n";
     }
 };
 
-class NoopLog
+struct NoopLog
 {
     static const LogCategory category = LogCategory::ANY;
 
@@ -56,13 +47,13 @@ class NoopLog
 
 
 template <LogCategory t_category>
-class ErrLog
+struct ErrLog
 {
     static const LogCategory category = t_category;
 
     void log(LogEntry t_entry) {
-        std::cerr << '[' << t_entry.file_name << ':' << t_entry.function_name << ':' << t_entry.line_number << ']'
-                  <<  t_entry.message.str ( ) << "\n";
+        std::cerr << '[' << t_entry.file_name << ':' << t_entry.function_name << ':' << t_entry.line_number << "] "
+                  <<  t_entry.message << "\n";
     }
 };
 
@@ -73,7 +64,7 @@ class LogKeeper : Logs ...
   private:
     bool m_debug = false;
     bool m_standard = false;
-    
+
   public:
 
     LogKeeper(bool t_debug = false, bool t_standard = true)
@@ -81,32 +72,33 @@ class LogKeeper : Logs ...
 
     template<typename MSG>
         void create_entry(MSG msg, const char* file, const char* function, int line_number, LogCategory category) {
-        LogEntry entry{std::string(file), std::string(function), line_number, msg};
-     
+        const char* last_slash = strrchr ( file, '/' );
+        last_slash = last_slash ? last_slash + 1 : file;
+        
+        LogEntry entry{std::string(last_slash), std::string(function), line_number, msg};
+
         (do_log(static_cast<Logs&>(*this), entry, category), ...);
     }
 
 
     template<typename Logger>
         void do_log(Logger && logger, LogEntry t_entry, LogCategory t_category) {
-        
-        if ( Logger::category == t_category || Logger::category == LogCategory::ANY ) {
+
+        if ( logger.category == t_category || logger.category == LogCategory::ANY ) {
 
             if (!m_debug and t_category == LogCategory::DEBUG){ return; }
             if (!m_standard and t_category != LogCategory::DEBUG){ return; }
-            
+
 
             logger.log(t_entry);
         }
 
-        
+
     }
 
-    
-    
+
+
 };
-
-
 
 
 #ifdef DEUBG_LOGGING
@@ -116,22 +108,20 @@ using standard_logger = LogKeeper<
     StdLog<LogCategory::WARNING>,
     ErrLog<LogCategory::ERROR>
     >;
-standard_logger LOGGER;
+inline standard_logger LOGGER;
 
-#define AL_DEBUG(MSG)   do{ ::alisp::logging::LOGGER.create_entry(MSG, __FILE__, __FUNCTION__, __LINE__, LogCategory::DEBUG); }while(false)
-#define AL_INFO(MSG)    do{ ::alisp::logging::LOGGER.create_entry(MSG, __FILE__, __FUNCTION__, __LINE__, LogCategory::INFO); }while(false)
-#define AL_WARNING(MSG) do{ ::alisp::logging::LOGGER.create_entry(MSG, __FILE__, __FUNCTION__, __LINE__, LogCategory::WARNING); }while(false)
-#define AL_ERROR(MSG)   do{ ::alisp::logging::LOGGER.create_entry(MSG, __FILE__, __FUNCTION__, __LINE__, LogCategory::ERROR); }while(false)
-
+#define AL_LOG(MSG, TAG) do{ ::alisp::logging::LOGGER.create_entry(MSG, __FILE__, __FUNCTION__, __LINE__, ::alisp::logging::LogCategory::TAG); }while(false)
 #else
 using standard_logger = LogKeeper<NoopLog>;
 
-#define AL_DEBUG(MSG)    do{(void)0;}while(false)
-#define AL_INFO(MSG)     do{(void)0;}while(false)
-#define AL_WARNING(MSG)  do{(void)0;}while(false)
-#define AL_ERROR(MSG)    do{(void)0;}while(false)
-
+#define AL_LOG(MSG, TAG) do{(void)0;}while(false)
 #endif
+
+
+#define AL_DEBUG(MSG)   AL_LOG(MSG, DEBUG)
+#define AL_INFO(MSG)    AL_LOG(MSG, INFO)
+#define AL_WARNING(MSG) AL_LOG(MSG, WARNING)
+#define AL_ERROR(MSG)   AL_LOG(MSG, ERROR)
 
 void init_logging(bool debug =  false, bool stand = true);
 
@@ -139,5 +129,3 @@ void init_logging(bool debug =  false, bool stand = true);
 
 
 }
-
-
