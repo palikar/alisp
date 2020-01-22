c9d18c69a91667f9767988dfe1430cab6fda556c
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-01-22 14:19:49 +0100

    Adding ideas about math stuff

--- src/alisp/src/modules/math.cpp
+++ src/alisp/src/modules/math.cpp
@@ -30,7 +30,19 @@ inline constexpr double PI = 3.14159265358979323846;
 inline constexpr double E = 2.71828182845904523536;
 inline constexpr double TAU = 2*PI;
 
+// inf
+// nan
+
 }
+
+// trigonometry
+// factorial
+// ceiling flooring et
+// gcd
+// logs
+// sqrt
+
+
 
 
 env::ModulePtr init_math(env::Environment*, eval::Evaluator*) {
