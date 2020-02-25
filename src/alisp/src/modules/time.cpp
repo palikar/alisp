5895d112e275f09311c1fc4d3b649be64676e533
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-02-25 12:28:15 +0100

    Finishing up the basic time module

--- src/alisp/src/modules/time.cpp
+++ src/alisp/src/modules/time.cpp
@@ -20,12 +20,14 @@
 #include <iomanip>
 #include <chrono>
 #include <ctime>
+#include <thread>
 
 namespace alisp
 {
 
 namespace details
 {
+
 namespace ch = std::chrono;
 
 typedef std::chrono::duration<ALObject::real_type> al_seconds;
@@ -33,25 +35,31 @@ typedef std::chrono::duration<ALObject::real_type> al_seconds;
 static constexpr int SYSTEM_CLOCK = 1;
 static constexpr int STEADY_CLOCK = 2;
 static constexpr int HIGH_RES_CLOCK = 3;
-// static constexpr int GPS_CLOCK = 4;
-// static constexpr int LOCAL_CLOCK = 5;
+
+static constexpr int clocks_per_sec = CLOCKS_PER_SEC;
+
 
 ALObjectPtr Ftime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *)
 {
     assert_size<0>(t_obj);
     std::time_t result = std::time(nullptr);
     return make_int(result);
-    
+}
+
+ALObjectPtr Fclock(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *)
+{
+    assert_size<0>(t_obj);
+    return make_int(std::clock());
 }
 
 ALObjectPtr Fnow(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
 {
     namespace ch = std::chrono;
-    
+
     assert_size<1>(t_obj);
     auto clock = eval->eval(t_obj->i(0));
     assert_int(clock);
-    
+
     switch (static_cast<int>(clock->to_int())) {
       case SYSTEM_CLOCK : {
           const auto time_now = al_seconds(ch::system_clock::now().time_since_epoch()).count();
@@ -70,20 +78,20 @@ ALObjectPtr Fnow(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
     }
 
     return nullptr;
-    
+
 }
 
 ALObjectPtr Fnow_ns(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
 {
     namespace ch = std::chrono;
-    
+
     assert_size<1>(t_obj);
     auto clock = eval->eval(t_obj->i(0));
     assert_int(clock);
-    
+
     switch (static_cast<int>(clock->to_int())) {
       case SYSTEM_CLOCK : {
-          return make_int(ch::system_clock::now().time_since_epoch().count());          
+          return make_int(ch::system_clock::now().time_since_epoch().count());
       }
 
       case STEADY_CLOCK : {
@@ -96,57 +104,191 @@ ALObjectPtr Fnow_ns(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval
     }
 
     return nullptr;
-    
+
 }
 
 ALObjectPtr Fgmtime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
 {
-    namespace ch = std::chrono;    
+    namespace ch = std::chrono;
     assert_size<1>(t_obj);
     auto time = eval->eval(t_obj->i(0));
     assert_int(time);
     std::time_t time_t(time->to_int());
     auto res = std::gmtime(&time_t);
-    return make_object(res->tm_sec, res->tm_min, res->tm_hour, res->tm_mday, res->tm_mon, res->tm_year, res->tm_wday, res->tm_yday, res->tm_isdst);   
+    return make_object(res->tm_sec, res->tm_min, res->tm_hour, res->tm_mday, res->tm_mon, res->tm_year, res->tm_wday, res->tm_yday, res->tm_isdst);
 }
 
 ALObjectPtr Flocaltime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
 {
-    namespace ch = std::chrono;    
+    namespace ch = std::chrono;
     assert_size<1>(t_obj);
     auto time = eval->eval(t_obj->i(0));
     assert_int(time);
     std::time_t time_t(time->to_int());
     auto res = std::localtime(&time_t);
-    return make_object(res->tm_sec, res->tm_min, res->tm_hour, res->tm_mday, res->tm_mon, res->tm_year, res->tm_wday, res->tm_yday, res->tm_isdst);   
+    return make_object(res->tm_sec, res->tm_min, res->tm_hour, res->tm_mday, res->tm_mon, res->tm_year, res->tm_wday, res->tm_yday, res->tm_isdst);
 }
 
+ALObjectPtr Fmktime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time_tup = eval->eval(t_obj->i(0));
+    assert_list(time_tup);
+    assert_size<9>(time_tup);
+    assert_numbers(time_tup);
 
+    std::tm tm;
+    tm.tm_sec = static_cast<int>(time_tup->i(0)->to_int());
+    tm.tm_min = static_cast<int>(time_tup->i(1)->to_int());
+    tm.tm_hour = static_cast<int>(time_tup->i(2)->to_int());
+    tm.tm_mday = static_cast<int>(time_tup->i(3)->to_int());
+    tm.tm_mon = static_cast<int>(time_tup->i(4)->to_int());
+    tm.tm_year = static_cast<int>(time_tup->i(5)->to_int());
+    tm.tm_wday = static_cast<int>(time_tup->i(6)->to_int());
+    tm.tm_yday = static_cast<int>(time_tup->i(7)->to_int());
+    tm.tm_isdst = static_cast<int>(time_tup->i(8)->to_int());
 
+    return make_int(std::mktime(&tm));
 }
 
+ALObjectPtr Fstrftime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_min_size<2>(t_obj);
+    auto time_tup = eval->eval(t_obj->i(1));
+    assert_list(time_tup);
+    assert_size<9>(time_tup);
+    assert_numbers(time_tup);
+
+    auto time_fmt = eval->eval(t_obj->i(0));
+    assert_string(time_fmt);
+
+    std::tm tm;
+    tm.tm_sec = static_cast<int>(time_tup->i(0)->to_int());
+    tm.tm_min = static_cast<int>(time_tup->i(1)->to_int());
+    tm.tm_hour = static_cast<int>(time_tup->i(2)->to_int());
+    tm.tm_mday = static_cast<int>(time_tup->i(3)->to_int());
+    tm.tm_mon = static_cast<int>(time_tup->i(4)->to_int());
+    tm.tm_year = static_cast<int>(time_tup->i(5)->to_int());
+    tm.tm_wday = static_cast<int>(time_tup->i(6)->to_int());
+    tm.tm_yday = static_cast<int>(time_tup->i(7)->to_int());
+    tm.tm_isdst = static_cast<int>(time_tup->i(8)->to_int());
+
+    char mbstr[512];
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#endif
+    if (auto size = std::strftime(mbstr, sizeof(mbstr), time_fmt->to_string().c_str(), &tm); size!= 0) {
+        return make_string(std::string(mbstr, size));
+    }
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+
+    return Qnil;
+}
+
+ALObjectPtr Fctime(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_max_size<1>(t_obj);
+    if (std::size(*t_obj) > 1) {
+        auto time = eval->eval(t_obj->i(0));
+        assert_int(time);
+        std::time_t time_t(time->to_int());
+        return make_string(std::ctime(&time_t));
+    }
+    auto time_t = std::time(nullptr);
+    return make_string(std::ctime(&time_t));
+}
+
+ALObjectPtr Fns(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time = eval->eval(t_obj->i(0));
+    assert_int(time);
+    auto res = ch::duration_cast<al_seconds>(ch::nanoseconds(time->to_int())).count();
+    return make_real(res);
+}
+
+ALObjectPtr Fms(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time = eval->eval(t_obj->i(0));
+    assert_int(time);
+    auto res = ch::duration_cast<al_seconds>(ch::milliseconds(time->to_int())).count();
+    return make_real(res);
+}
+
+ALObjectPtr Fs(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time = eval->eval(t_obj->i(0));
+    assert_int(time);
+    auto res = ch::duration_cast<al_seconds>(ch::seconds(time->to_int())).count();
+    return make_real(res);
+}
+
+ALObjectPtr Fhr(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time = eval->eval(t_obj->i(0));
+    assert_int(time);
+    auto res = ch::duration_cast<al_seconds>(ch::hours(time->to_int())).count();
+    return make_real(res);
+}
+
+ALObjectPtr Fsleep(ALObjectPtr t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    namespace ch = std::chrono;
+    assert_size<1>(t_obj);
+    auto time = eval->eval(t_obj->i(0));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(time->to_int()));
+
+    return Qt;
+}
+
+}
 
 env::ModulePtr init_time(env::Environment *, eval::Evaluator *)
 {
-
     auto Mtime    = module_init("time");
     auto time_ptr = Mtime.get();
 
     module_doc(time_ptr, R"(The `time` module provides utility functions for working with time
 and dates.)");
 
-
-
     module_defconst(time_ptr, "system-clock", make_int(details::SYSTEM_CLOCK));
     module_defconst(time_ptr, "steady-clock", make_int(details::STEADY_CLOCK));
     module_defconst(time_ptr, "high-res-clock", make_int(details::HIGH_RES_CLOCK));
 
-    module_defun(time_ptr, "t-ctime", &details::Ftime);
+    module_defconst(time_ptr, "high-res-clock", make_int(details::clocks_per_sec));
+    
+    module_defun(time_ptr, "t-time", &details::Ftime);
+    module_defun(time_ptr, "t-ctime", &details::Fctime);
     module_defun(time_ptr, "t-gmtime", &details::Fgmtime);
     module_defun(time_ptr, "t-localtime", &details::Flocaltime);
+    module_defun(time_ptr, "t-mktime", &details::Fmktime);
+    module_defun(time_ptr, "t-process-time", &details::Fclock);
+    module_defun(time_ptr, "t-strftime", &details::Fstrftime);
 
-    module_defun(time_ptr, "t-now", &details::Fnow);
-    module_defun(time_ptr, "t-now-ns", &details::Fnow_ns);
+    module_defun(time_ptr, "t-clock-time", &details::Fnow);
+    module_defun(time_ptr, "t-clock-time-ns", &details::Fnow_ns);
+    
+    module_defun(time_ptr, "t-ns", &details::Fns);
+    module_defun(time_ptr, "t-ms", &details::Fms);
+    module_defun(time_ptr, "t-s", &details::Fs);
+    module_defun(time_ptr, "t-hr", &details::Fhr);
+
+    module_defun(time_ptr, "t-sleep", &details::Fsleep);
 
     return Mtime;
 }
