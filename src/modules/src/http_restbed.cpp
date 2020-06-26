aa5fc487f0af500b01cb76bd2f9242397b908c04
Arnaudov, Stanislav Tomov <stanislav.arnaudov@iosb.fraunhofer.de>
2020-06-26 17:26:54 +0200

    Set up some basic server things for the new library

--- src/modules/src/http_restbed.cpp
+++ src/modules/src/http_restbed.cpp
@@ -37,52 +37,147 @@ using namespace alisp;
 namespace detail
 {
 
-// inline management::Registry<uWS::App, 0x07> server_registry;
+struct Server
+{
+    std::vector<restbed::Resource> g_resources;
+    restbed::Settings g_settings;
+    std::unique_ptr<restbed::Service> g_server;
+    
 
-// struct server_start
-// {
 
-//     static constexpr bool managed    = false;
-//     static constexpr bool has_future = false;
+};
 
-//     ALObjectPtr g_id;
 
-//     server_start(ALObjectPtr id) : g_id(std::move(id)) {}
+inline management::Registry<uWS::App, 0x08> server_registry;
 
-//     ALObjectPtr operator()(async::AsyncS *async) const
-//     {
+struct server_start
+{
 
-//     }
-// };
+    static constexpr bool managed    = false;
+    static constexpr bool has_future = false;
+
+    ALObjectPtr g_id;
+
+    server_start(ALObjectPtr id) : g_id(std::move(id)) {}
+
+    ALObjectPtr operator()(async::AsyncS *async) const
+    {
+        
+        auto t = std::thread([g_id = g_id, async = async]() {
+            auto id = object_to_resource(g_id);
+            auto &server = detail::server_registry[id];
+
+            
+
+            for (auto& resoruce : server.g_resoruces)
+            {
+                server.g_service->publish(res);
+            }
+            
+            server.g_service->start(server.g_settings);
+            async->async_reset_pending();
+        });
+
+        t.detach();
+        return Qt;
+
+    }
+};
+
 
 }  // namespace detail
 
 
 ALObjectPtr Fserver(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
 {
+    auto new_id = detail::server_registry.emplace_resource()->id;
 
+    auto &server = detail::server_registry[new_id];
+    server.g_service = std::make_unique<restbed::Service>();
+    
+    return resource_to_object(new_id);
+}
+
+ALObjectPtr Fserver_port(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval, 0));
+    auto port  = AL_EVAL(t_obj, eval, 1);
+        
+    detail::server_registry[id].g_settings.set_port(port->to_int());
+    
+    return Qt;
+}
+
+ALObjectPtr Fserver_root(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval, 0));
+    auto port  = AL_EVAL(t_obj, eval, 1);
+        
+    detail::server_registry[id].g_settings.set_root(port->to_string());
+    
+    return Qt;
+}
+
+ALObjectPtr Fserver_address(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval, 0));
+    auto address  = AL_EVAL(t_obj, eval, 1);
+        
+    detail::server_registry[id].g_settings.set_address(address->to_string());
+    
+    return Qt;
+}
+
+ALObjectPtr Fserver_default_header(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
+{
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval, 0));
+    auto header  = AL_EVAL(t_obj, eval, 1);
+    auto value  = AL_EVAL(t_obj, eval, 2);
+        
+    detail::server_registry[id].g_settings.set_default_header(header->to_string(), value->to_string());
+    
+    return Qt;
 }
 
 ALObjectPtr Fget(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
 {
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval 0));
+    auto route = AL_EVAL(t_obj, eval 1);
+    auto fun   = AL_EVAL(t_obj, eval 2);
 
+    auto res = restbed::Resource();
+    resource->set_path(route->to_string());
+    resource->set_method_handler( "GET", [](const shared_ptr< Session > session) {
+        
+    });
 
+    detail::server_registry[id].g_resources.push_back(std::move(resource));
 
+    return Qt;
 }
 
 ALObjectPtr Fpost(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
 {
-    auto id    = object_to_resource(eval->eval(t_obj->i(0)));
-    auto route = eval->eval(t_obj->i(1));
-    auto fun   = eval->eval(t_obj->i(2));
+    auto id    = object_to_resource(AL_EVAL(t_obj, eval 0));
+    auto route = AL_EVAL(t_obj, eval 1);
+    auto fun   = AL_EVAL(t_obj, eval 2);
 
-    
+    auto res = restbed::Resource();
+    resource->set_path(route->to_string());
+    resource->set_method_handler( "POST", [](const shared_ptr< Session > session) {
+        
+    });
 
-    return Qnil;
+    detail::server_registry[id].g_resources.push_back(std::move(resource));
+
+    return Qt;
 }
 
 ALObjectPtr Fstart(const ALObjectPtr &t_obj, env::Environment *, eval::Evaluator *eval)
 {
+    eval->async().async_pending();
+    return async::dispatch<detail::server_start>(eval->async(), eval->eval(t_obj->i(0)));
+
 
 }
 
